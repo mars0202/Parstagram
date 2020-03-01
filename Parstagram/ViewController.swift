347f4a600e22aa2f847a6029291812d78491f6d4
Manish Rajendran <rajend16@msu.edu>
2020-03-01 12:55:24 -0500

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Manish Rajendran on 3/1/20.
+//  Copyright © 2020 Manish Rajendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
