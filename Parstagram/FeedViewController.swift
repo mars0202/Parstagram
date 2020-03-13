66294367345e2bf4696aec6812ad675100f72a80
Manish Rajendran <rajend16@msu.edu>
2020-03-13 18:00:23 -0400

    init commit

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+//    var posts = [PFObject]()
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
     
 
