f0b9df958e9e11447634b95c5a01cf4d56179c5e
Manish Rajendran <rajend16@msu.edu>
2020-03-18 19:24:03 -0400

    finished querying TODO: Auto-layout and remove hard coded tableView row height vals

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-//    var posts = [PFObject]()
+    var posts = [PFObject]()
 
     
     @IBOutlet weak var tableView: UITableView!
@@ -19,14 +21,47 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        // querying the parse server
+        let query = PFQuery(className: "Posts")
+        query.includeKeys(["author", "comments", "comments.author"])
+        query.limit = 20
+        
+        // getting the posts from the query, storing data, and reloading tableView
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+//        (posts.count)
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+//        cell.usernameLabel.text = "Manish Rajendran"
+        cell.captionLabel.text = post["caption"] as! String
+//        cell.captionLabel.text = "Hello!"
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+
+        return cell
     }
     
 
